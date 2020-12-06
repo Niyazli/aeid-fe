445e4f5245baf9b10f3a15b2a128c7606fbd3c4e
farid.niyazli <farid.niyazli@smartsolutions.az>
2020-12-06 16:02:27 +0400

    localization

--- src/app/modules/routing/about/about.module.ts
+++ src/app/modules/routing/about/about.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import {LocalizationModule} from '../../localization';
 
 
 @NgModule({
   declarations: [AboutPageComponent],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    LocalizationModule
   ]
 })
 export class AboutModule { }
