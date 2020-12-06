f062f423b56b8da6e67ade2cdb254ecec57a779d
farid.niyazli <farid.niyazli@smartsolutions.az>
2020-12-06 14:53:13 +0400

    help page

--- src/app/modules/routing/help/help-routing.module.ts
+++ src/app/modules/routing/help/help-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HelpPageComponent} from './pages/help-page/help-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HelpPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HelpRoutingModule { }
+export class HelpRoutingModule {}
--- src/app/modules/routing/help/help.module.ts
+++ src/app/modules/routing/help/help.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HelpRoutingModule } from './help-routing.module';
-
+import {HelpRoutingModule} from './help-routing.module';
+import {HelpPageComponent} from './pages/help-page/help-page.component';
+import { HelpBoxComponent } from './components/help-box/help-box.component';
+import {MaterialModule} from '../../material/material.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HelpRoutingModule
-  ]
+  declarations: [HelpPageComponent, HelpBoxComponent],
+  imports: [CommonModule, HelpRoutingModule, MaterialModule],
 })
-export class HelpModule { }
+export class HelpModule {}
